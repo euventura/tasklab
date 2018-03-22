5b9a1de03a805c003a2b007854e54f1a2b8208c3
Victor Ventura <victor.ventura@convenia.com.br>
2018-03-22 13:53:04 -0300

    add current tag

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -162,7 +162,7 @@ class DefaultController extends Controller
 
         });
 
-        if ($currentTagState->count() === 1) {
+        if ($currentTagState->count() > 0) {
             $task = Task::findOrFail($taskId);
             $task->current_tag = $currentTagState->first()['name'];
             $task->save();
