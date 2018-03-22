abcac59a23cbdc1006f34d72b06cfeace407ff94
Victor Ventura <victor.ventura@convenia.com.br>
2018-03-22 14:06:56 -0300

    changes validation

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -162,7 +162,7 @@ class DefaultController extends Controller
 
         });
 
-        Log::info('states', $currentTagState);
+        Log::info('states', $currentTagState->toArray());
         if ($currentTagState->count() > 0) {
             $task = Task::findOrFail($taskId);
             $task->current_tag = $currentTagState->first()['name'];
